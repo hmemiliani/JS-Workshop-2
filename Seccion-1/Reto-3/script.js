943cc1c8cbcc092093a7f9fb2cb254b3c0ded324
hmemiliani <harold_medranoe@hotmail.com>
2024-04-17 15:04:17 -0500

    update seccion-1 Reto-3

--- Seccion-1/Reto-3/script.js
+++ Seccion-1/Reto-3/script.js
@@ -1,33 +1,44 @@
 let presupuestoInicial = parseFloat(prompt("Ingrese su presupesto inicial:" ));
 let costoViaje = parseFloat(prompt("Ingrese el costo total estimado del viaje:"));
-let precioArticuloExtra = parseFloat(prompt("Ingrese el precio del artículo extra que desea comprar:"));
-let presupuestoRestante = presupuestoInicial - costoViaje;
-let umbral = 100; 
+let articulosElegidos = [];
 
-if (presupuestoRestante >= precioArticuloExtra + umbral) {
-    console.log("Puedes comprar un artículo extra!");
+while (true) {
 
-    let opcionesArticulos = [
-        { nombre: "Libro", precio: 20 },
-        { nombre: "Cámara", precio: 200 },
-        { nombre: "Botella de vino", precio: 50 }
-    ];
+    let nombreArticulo = prompt("Ingrese el nombre del Articulo que quiere llevar:");
+    let precioArticulo = parseFloat(prompt("Ingrese el precio del artículo:"));
 
-    let opcionMasEconomica = null;
-    let precioMasEconomico = presupuestoRestante;
+    if (!isNaN(precioArticulo)) {
+        if(presupuestoInicial - costoViaje >= precioArticulo) {
+            articulosElegidos.push(
+                {nombre: nombreArticulo, precio: precioArticulo}
+            );
 
-    for (let i = 0; i < opcionesArticulos.length; i++) {
-        if (opcionesArticulos[i].precio <= precioMasEconomico) {
-            opcionMasEconomica = opcionesArticulos[i];
-            precioMasEconomico = opcionesArticulos[i].precio;
+        }  else {
+            console.log("El precio del artículo excede su presupuesto restante. Por favor, elija otro artículo.");
         }
-    }
-
-    if (opcionMasEconomica) {
-        console.log("La opción más económica es:", opcionMasEconomica.nombre);
     } else {
-        console.log("Lo siento, no hay opciones asequibles dentro de tu presupuesto.");
+        console.log("Por favor, ingrese un precio válido para el artículo.");
     }
+    let respuesta = confirm("¿Desea agregar otro artículo?");
+    if (respuesta == false){
+        break;
+    }
+    
+}
+console.log("Lista de artículos elegidos:");
+articulosElegidos.forEach(articulo => {
+    console.log(`- ${articulo.nombre}: ${articulo.precio}`);
+});
+
+
+// Calculo el presupuesto restante
+let presupuestoRestanteDespuesCompras = presupuestoInicial - costoViaje - articulosElegidos.reduce((total, articulo) => total + articulo.precio, 0);
+
+console.log(`Presupuesto restante después de comprar los artículos: ${presupuestoRestanteDespuesCompras.toFixed(2)}`); //formatea un numero flotante a la cantidad de numeros deseada
+
+// posibilidad de artículo extra
+if (presupuestoRestanteDespuesCompras >= 0) {
+    console.log("Te queda algo de presupuesto");
 } else {
-    console.log("Deberías reconsiderar la compra de un artículo extra para no exceder tu presupuesto.");
+    console.log("Te quedaste sin presupuesto, no puedes comprar nada extra");
 }
