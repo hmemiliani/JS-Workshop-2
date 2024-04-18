47674e87e795111e18fd5889fa91cc49a9ecd3c8
hmemiliani <harold_medranoe@hotmail.com>
2024-04-18 16:15:45 -0500

    update some things

--- Seccion-2/Reto-1/script.js
+++ Seccion-2/Reto-1/script.js
@@ -6,34 +6,34 @@ if (estadoDeAnimo <= 3 ){
     alert("Es mejor que hoy no hagas nada y te quedes descansando");
 }
 
-if (estadoDeAnimo > 3 &&  estadoDeAnimo < 6 && job==true && clima == true){
+if (estadoDeAnimo > 3 &&  estadoDeAnimo < 6 && job && clima){
     alert("Aunque no estas al 100% el clima esta bello, ve a trabajar, te cambiara el estado de animo");
 }
 
-if (estadoDeAnimo > 3 &&  estadoDeAnimo < 6 && job==true && clima == false){
+if (estadoDeAnimo > 3 &&  estadoDeAnimo < 6 && job && !clima){
     alert("El clima no esta muy bueno, ademas de que no estas al 100%, mejor trabaja desde casa hoy");
 }
 
-if (estadoDeAnimo > 3 &&  estadoDeAnimo < 6 && job==false && clima == true){
+if (estadoDeAnimo > 3 &&  estadoDeAnimo < 6 && !job && clima){
     alert("Mejor quedate descansando, el clima esta bien, pero si no estas al 100 mejor descansar, mira que hay de nuevo en netflix");
 }
 
-if (estadoDeAnimo > 3 &&  estadoDeAnimo < 6 && job==false && clima == false){
+if (estadoDeAnimo > 3 &&  estadoDeAnimo < 6 && !job && !clima){
     alert("El clima esta perfecto para no hacer nada, a dormir y ver series");
 }
 
-if (estadoDeAnimo >= 6  && job==true && clima == true){
+if (estadoDeAnimo >= 6  && job && clima){
     alert("Estas de buen animo, el clima se ve genial! ve a trabajar, hoy sera un buen dia");
 }
 
-if (estadoDeAnimo >= 6  && job==true && clima == false){
+if (estadoDeAnimo >= 6  && job && !clima){
     alert("Tienes buen animo, pero el clima no ayuda, puedes intentar ir a trabajar o quedarte, tu decide, que nada dañe tu dia");
 }
 
-if (estadoDeAnimo >= 6  && job==false && clima == true){
+if (estadoDeAnimo >= 6  && !job && clima){
     alert("Tienes buen animo y no hay trabajo! puedes salir a pasear o hacer algo que desees");
 }
 
-if (estadoDeAnimo >= 6  && job==false && clima == false){
+if (estadoDeAnimo >= 6  && !job && !clima){
     alert("es un dia perfecto para quedarse en casa descansando, pasalo bien!");
 }
